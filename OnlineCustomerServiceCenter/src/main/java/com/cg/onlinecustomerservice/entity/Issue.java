ed2b1613828435789b675fb2fe3667f10dc0a2d1
HP <HP@LAPTOP-OCLISKAK>
2021-04-02 10:22:14 +0530

    issue changes

--- OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/entity/Issue.java
+++ OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/entity/Issue.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+// issue
 @Entity
 @Table(name="issue")
 public class Issue {

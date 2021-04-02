8470c03e8c305c542cec5d47c966b52983e83f40
Nisha <nisha.kamath.122@gmail.com>
2021-04-02 15:18:04 +0530

    Operator Modified

--- OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/entity/Operator.java
+++ OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/entity/Operator.java
@@ -12,8 +12,6 @@ import javax.persistence.Table;
 @Entity
 @Table(name="operator")
 public class Operator {
-	
-	public class Department {
 		@Id
 		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		@Column(name="operatorId")
@@ -30,7 +28,6 @@ public class Operator {
 		
 		@Column
 		private String city;
-<<<<<<< HEAD
 		
 		@OneToOne(mappedBy="operator")
 		private Solution solution;
@@ -45,23 +42,15 @@ public class Operator {
 		
 		public Operator()
 		{}
-
+		
 		public Operator(int operatorId, String firstName, String lastName, String email, String mobile, String city) {
-=======
-		public Department()
-		{
-			
-		}
-		public Department(int operatorId, String firstName, String lastName, String email, String mobile, String city) {
->>>>>>> branch 'master' of https://github.com/jr5g3OnlineCustomerService/OnlineCustomerServiceCenter.git
 			super();
 			this.operatorId = operatorId;
 			this.firstName = firstName;
 			this.lastName = lastName;
 			this.email = email;
 			this.mobile = mobile;
-			this.city = city;
-		}
+			this.city = city;}
 
 		public int getOperatorId() {
 			return operatorId;

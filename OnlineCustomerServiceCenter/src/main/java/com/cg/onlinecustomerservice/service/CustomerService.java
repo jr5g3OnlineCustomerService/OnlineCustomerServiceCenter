df2dd8046cc39be7b4daea6f842a417413759b74
dell <dell@DESKTOP-39F187M>
2021-04-03 09:45:59 +0530

    push

--- OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/service/CustomerService.java
+++ OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/service/CustomerService.java
@@ -1,13 +1,6 @@
 package com.cg.onlinecustomerservice.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.cg.onlinecustomerservice.dao.CustomerDao;
-
-import com.cg.onlinecustomerservice.entity.Customer;
-import com.cg.onlinecustomerservice.entity.Issue;
-import com.cg.onlinecustomerservice.entity.Solution;
 
 @Service
 public class CustomerService {

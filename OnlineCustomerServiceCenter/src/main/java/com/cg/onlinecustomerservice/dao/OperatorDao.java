6e63a876e478e6968219facfbf950214943f8e21
Namith <nmith5610@gmail.com>
2021-04-03 10:28:45 +0530

    Updated AdminService

--- OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/dao/OperatorDao.java
+++ OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/dao/OperatorDao.java
@@ -1,10 +1,13 @@
 package com.cg.onlinecustomerservice.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.cg.onlinecustomerservice.entity.Department;
 import com.cg.onlinecustomerservice.entity.Operator;
 @Repository
 public interface OperatorDao extends JpaRepository<Operator , Integer>{
-
+	@Query(value = "from Operator operator where operator.operatorID=?1")
+	public Operator findOperatorById(int code);
 }
--- OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/service/AdminService.java
+++ OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/service/AdminService.java
@@ -1,11 +1,14 @@
 package com.cg.onlinecustomerservice.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cg.onlinecustomerservice.dao.DepartmentDao;
 import com.cg.onlinecustomerservice.dao.OperatorDao;
 import com.cg.onlinecustomerservice.entity.Department;
+import com.cg.onlinecustomerservice.entity.Operator;
 
 @Service
 public class AdminService {
@@ -47,5 +50,43 @@ public Department findDepartmentById(int id) {
 	return deptDao.getDeptById(id);
 	//return deptDao.findById(id);
 }
+public boolean addOperator(Operator operator){
+	boolean flag=true;
+	if(flag){
+	operatorDao.save(operator);
+}
+	else{
+		flag=false;
+	}
+	return flag;
+}
+public boolean removeOperator(int operatorID){ 
+	boolean flag=true;
+	if(flag){
+	operatorDao.deleteById(operatorID);
+}
+	else{
+		flag=false;
+	}
+	return flag;
+	}
+public Operator modifyOperator(Operator operator){
 
+	int id=operator.getOperatorId();
+	Operator oper=operatorDao.findById(id).get();
+	oper.setFirstName(operator.getFirstName());
+	oper.setLastName(operator.getLastName());
+	oper.setMobile(operator.getMobile());
+	oper.setCity(operator.getCity());
+	operatorDao.save(oper);
+	return oper;
+}
+public Operator findOperator(int id) {
+	return operatorDao.findOperatorById(id);
+	//return operatorDao.findById(id);
+}
+public List<Operator> findAllOperators(){
+	List<Operator> operators = operatorDao.findAll();
+	return operators;
+}
 }

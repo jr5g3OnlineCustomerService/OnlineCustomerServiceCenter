3a8061e88ae938c2e1df1520255f8878d3aae083
neshma thimmaiah <neshma thimmaiah@LAPTOP-NVPC1OG3.lan>
2021-04-06 16:05:38 +0530

    Exception classes are created

---
+++ OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/utils/DuplicateCustomerException.java
@@ -0,0 +1,5 @@
+package com.cg.onlinecustomerservice.utils;
+
+public class DuplicateCustomerException extends Exception{
+
+}
---
+++ OnlineCustomerServiceCenter/src/main/java/com/cg/onlinecustomerservice/utils/IssueNotFoundException.java
@@ -0,0 +1,5 @@
+package com.cg.onlinecustomerservice.utils;
+
+public class IssueNotFoundException extends Exception{
+
+}

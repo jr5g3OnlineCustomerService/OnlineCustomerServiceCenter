88f11cbdba2d7bb3e2a987e1c772fd0b383912a8
Nikhil298 <naik.nikhil298@gmail.com>
2021-04-06 15:31:55 +0530

    added somemore test cases for adminservice

--- OnlineCustomerServiceCenter/src/test/java/OnlineCustomerServiceCenter/LibraryTest.java
+++ OnlineCustomerServiceCenter/src/test/java/OnlineCustomerServiceCenter/LibraryTest.java
@@ -14,7 +14,10 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.cg.onlinecustomerservice.dao.DepartmentDao;
+import com.cg.onlinecustomerservice.dao.OperatorDao;
+import com.cg.onlinecustomerservice.dto.OperatorDto;
 import com.cg.onlinecustomerservice.entity.Department;
+import com.cg.onlinecustomerservice.entity.Operator;
 import com.cg.onlinecustomerservice.service.AdminService;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -23,6 +26,8 @@ public class LibraryTest {
 	AdminService service;
 	@Mock
 	DepartmentDao dDao;
+	@Mock
+	OperatorDao oDao;
 	
 	@Before
 	public void init() {
@@ -44,4 +49,44 @@ public class LibraryTest {
 		Assertions.assertEquals(3, list.size()); 
 		Mockito.verify(dDao,Mockito.times(1)).findAll();
 	}
+	@Test
+	public void testGetAllOperator() {
+		List<Operator> operators=new ArrayList<Operator>();
+		operators.add(new Operator(1,"Nikhil","naik","sds","sds","sds"));
+		Mockito.when(oDao.findAll()).thenReturn(operators);
+		List<Operator> list = service.findAllOperators();
+		Assertions.assertEquals(1, list.size());
+		Mockito.verify(oDao,Mockito.times(1)).findAll();
+	}
+	@Test
+	public void testAddDepartment() {
+		Department department=new Department();
+		department.setDepartmentID(1);
+		department.setDepartmentName("IT");
+		service.addDepartment(department);
+		Assertions.assertNotNull(department.getDepartmentID());
+		Mockito.verify(dDao, Mockito.times(1)).save(department);
+	}
+	@Test
+	public void testRemoveDepartment() {
+		Department department=new Department();
+		service.removeDepartment(1);
+		Assertions.assertNotNull(department.getDepartmentID());
+		Mockito.verify(dDao, Mockito.times(1)).deleteById(1);
+	}
+	@Test
+	public void testfindDepartmentById() {
+		Department department=new Department();
+		service.findDepartmentById(1);
+		Assertions.assertNotNull(department.getDepartmentID());
+		Mockito.verify(dDao, Mockito.times(1)).getDeptById(1);
+	}
+	/*@Test
+	public void testUpdateDepartment() {
+		Department department=new Department();
+		department.setDepartmentName("IT");
+		service.modifyDepartment(department);
+		Assertions.assertNotNull(department.getDepartmentID());
+		Mockito.verify(dDao, Mockito.times(1)).save(department);
+	}*/
 }
